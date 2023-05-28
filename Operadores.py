0251546cfe71d36e5b7a8a9a57ec3d965784e58d
Fernando Ozaki <ozak.pgm@gmail.com>
2023-05-27 21:09:19 -0300

    terceira aula.,

---
+++ Operadores.py
@@ -0,0 +1,27 @@
+#Operações matemáticas (Aritméticas)
+
+numero1 = 10
+numero2 = 20
+
+print (numero1 + numero2) #soma
+print (numero1 - numero2) #subtração
+print (numero1 * numero2) #multiplicação
+print (numero1 / numero2) #Divisão
+print (numero1 // numero2) #Divisão inteira (descarta o numero após a virgula)
+print (20 // 3)
+print (20 % 3) #resto da divisão
+print (2 ** 3) #exponenciação (primeiro numero elevado pelo segundo)
+
+#Operadores Booleanos
+idade1 = 10
+idade2 = 15
+altura1 = 1.77
+altura2 = 1.65
+altura3 = altura2
+
+print (idade1 > idade2)
+print (idade1 <= idade2)
+print ('Python' == 'python')
+print ('banana' != 'abacaxi')
+print (altura1 >= altura2)
+print (altura2 > altura3) 
