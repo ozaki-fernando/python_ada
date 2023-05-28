1d35b718ed375930eb49a6568decfef953790048
Fernando Ozaki <ozak.pgm@gmail.com>
2023-05-28 09:16:24 -0300

    aula de Conversao_de_tipos

---
+++ Conversao_de_tipor.py
@@ -0,0 +1,15 @@
+# CONVERSÃO DE TIPOS
+
+idade = '26'
+
+idade_int = int(idade)
+
+print (idade_int, type(idade_int))
+
+#int()
+#str()
+#float()
+#bool()
+
+altura = float(input('Informe a sua altura: '))
+print (altura, type(altura))
